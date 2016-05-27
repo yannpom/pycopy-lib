garybake/micropython-lib

umqtt: Add pubsub example.

umqtt/example_pubsub.py
@@ -1,0 +1,11 @@
+from uqmtt import MQTTClient
+
+c = MQTTClient("mcli", "localhost")
+c.connect()
+c.publish(b"foo", b"hello")
+c.subscribe(b"foo")
+while 1:
+    #print(c.wait_msg())
+    print(c.check_msg())
+    time.sleep(1)
+c.disconnect()

yannpom/pycopy-lib

umqtt/example_pubsub.py
@@ -1,0 +1,12 @@
+import time
+from umqtt import MQTTClient
+
+c = MQTTClient("mcli", "localhost")
+c.connect()
+c.publish(b"foo", b"hello")
+c.subscribe(b"foo")
+while 1:
+    #print(c.wait_msg())
+    print(c.check_msg())
+    time.sleep(1)
+c.disconnect()